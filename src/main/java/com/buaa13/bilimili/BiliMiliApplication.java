7f7a6c203ce925bada40df19dfd420705f8e6ade
PengXinyang <148336099@qq.com>
2024-07-24 21:23:28 +0800

    V0.0.1

---
+++ src/main/java/com/buaa13/bilimili/BiliMiliApplication.java
@@ -0,0 +1,13 @@
+package com.buaa13.bilimili;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiliMiliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiliMiliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buaa13/bilimili/BiliMiliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buaa13.bilimili;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiliMiliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
